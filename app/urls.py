d8c700cb5b31776558bb6af5d52d0ef5bfb88955
Ivan <vanya.120614@gmail.com>
2026-05-29 17:56:36 +0300

    first commit

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('vacancy/<int:vacancy_id>/', views.vacancy_detail, name='vacancy_detail'),
+]
